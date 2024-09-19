6ac6a1de016294c5c68088970506593efd560184
CSLearner07 <117090454+CSLearner07@users.noreply.github.com>
2024-09-19 16:02:24 +0530

    lecture 2

---
+++ Lecture 2/lecture2.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a= 'a';
+    cout << a << endl;
+    char ch= 98;
+    cout << ch << endl;
+
+
+    return 0;
+}
+
+// In this lecture we have written this code to understand type casting 
+//If you understand the output clearly please search for ASCII table and learn about it 
