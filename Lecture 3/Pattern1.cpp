d146735d8df419d9e7922da3add8f5a3a24036b9
CSLearner07 <117090454+CSLearner07@users.noreply.github.com>
2024-09-19 16:13:54 +0530

    lecture 3

---
+++ Lecture 3/Pattern1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int n ;
+    cout << "Enter the number of stars you want to print : " <<endl ;
+    cin >> n;
+    int i = 1;
+    while (i <=n){
+        int j = 1;
+        while(j<=n){
+            cout<<" * ";
+            j = j + 1;
+        }
+        cout << endl;
+        i++;
+    }
+
+
+}
