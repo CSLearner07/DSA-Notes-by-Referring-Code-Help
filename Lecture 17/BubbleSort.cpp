c9dd7f344dacea4b0407ae3a3ce4d4732fbd4cb6
CSLearner07 <117090454+CSLearner07@users.noreply.github.com>
2024-12-02 20:44:11 +0530

    Lecture 17

---
+++ Lecture 17/BubbleSort.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void bubbleSort(vector<int>& arr, int n)
+{
+    for (int i = 1; i < n; i++) {
+        for (int j = 0; j < n - i; j++) {
+            if (arr[j] > arr[j + 1]) {
+                swap(arr[j], arr[j + 1]);
+            }
+        }
+    }
+}
+
+int main()
+{
+    int n;
+    cout << "Enter the number of elements: ";
+    cin >> n;
+
+    vector<int> arr(n);
+    cout << "Enter the elements: ";
+    for (int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    bubbleSort(arr, n);
+
+    cout << "Sorted array: ";
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
+
+
+/*
+
+Output : 
+
+Enter the number of elements: 5
+Enter the elements: 
+5
+1
+4
+2
+8
+Sorted array: 1 2 4 5 8
+
+*/
